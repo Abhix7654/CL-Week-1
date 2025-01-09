3a1f4fb1bfc597387b4d21c069759aa2cc52e31b
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-01-09 15:19:20 +0530

    Problem 3

---
+++ Level-1/Problem3.java
@@ -0,0 +1,10 @@
+class Problem3{
+public static void main(String[] args){
+
+float distance = 10.8f;
+float miles=distance*1.6f;
+System.out.print("The distance " + distance + "km in miles is "+miles);
+
+
+}
+}
