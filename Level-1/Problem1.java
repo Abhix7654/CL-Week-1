bb50f2fd014abaeb61d2cae8defe1b4dace1dc98
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-01-09 15:14:19 +0530

    problem1

---
+++ Level-1/Problem1.java
@@ -0,0 +1,11 @@
+class Problem1{
+public static void main(String[] args){
+
+//Harry's birth year
+int birthDate = 2000;
+
+//Calculation of Harry's current age
+System.out.print("Harry's age in 2024 is " + (2024-birthDate));
+
+}
+}
