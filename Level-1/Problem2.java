4587cd8ef280d517193b3838ec950ebf576d614f
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-01-09 15:17:38 +0530

    problem 2

---
+++ Level-1/Problem2.java
@@ -0,0 +1,15 @@
+class Problem2{
+public static void main(String[] args){
+
+//Sam's 3 subject marks
+int math = 94;
+int physics = 95;
+int chemistry =96;
+
+//Calculation of average 
+float average = (math+physics+chemistry)/3f;//converting from int to float
+
+System.out.print("Sam's average mark in PCM is " + average);
+
+}
+}
